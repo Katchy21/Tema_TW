b2c33c4e937da5ad82cb0840c08366636039cc84
Katchy21 <hulkras@gmail.com>
2024-11-22 13:07:41 +0200

    form tab

--- proiect_1/proiect1.js
+++ proiect_1/proiect1.js
@@ -2,28 +2,36 @@ function revealHomeArticle(event) {
     const homeArticle = document.getElementById("home");
     const workArticle = document.getElementById("work");
     const contactArticle = document.getElementById("contact");
+
     workArticle.style.opacity = '0';
     workArticle.style.visibility = 'hidden';
     contactArticle.style.opacity = '0';
     contactArticle.style.visibility = 'hidden';
+    
     setTimeout(function () {
         homeArticle.style.opacity = '1';
         homeArticle.style.visibility = 'visible';
     },500);
+    
+    
 }
 
 function revealWorkArticle(event) {
     const homeArticle = document.getElementById("home");
     const workArticle = document.getElementById("work");
-    const contactArticle = document.getElementById("contact");
+    const contactArticle = document.getElementById("contact"); 
+
     contactArticle.style.opacity = '0';
     contactArticle.style.visibility = 'hidden';
     homeArticle.style.opacity = '0';
     homeArticle.style.visibility = 'hidden';
+
     setTimeout(function () {
         workArticle.style.opacity = '1';
         workArticle.style.visibility = 'visible';
     },500);
+    
+    
 }
 
 function revealContactArticle(event) {
