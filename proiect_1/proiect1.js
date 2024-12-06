bc713696d4202f366b55ce80857fae4b49b1319f
Katchy21 <hulkras@gmail.com>
2024-12-06 17:01:27 +0200

    projects tab

--- proiect_1/proiect1.js
+++ proiect_1/proiect1.js
@@ -1,12 +1,15 @@
 function revealHomeArticle(event) {
     const homeArticle = document.getElementById("home");
     const workArticle = document.getElementById("work");
+    const projectsArticle = document.getElementById("projects");
     const contactArticle = document.getElementById("contact");
 
     workArticle.style.opacity = '0';
     workArticle.style.visibility = 'hidden';
     contactArticle.style.opacity = '0';
     contactArticle.style.visibility = 'hidden';
+    projectsArticle.style.opacity = '0';
+    projectsArticle.style.visibility = 'hidden';
     
     setTimeout(function () {
         homeArticle.style.opacity = '1';
@@ -19,12 +22,15 @@ function revealHomeArticle(event) {
 function revealWorkArticle(event) {
     const homeArticle = document.getElementById("home");
     const workArticle = document.getElementById("work");
-    const contactArticle = document.getElementById("contact"); 
+    const projectsArticle = document.getElementById("projects");
+    const contactArticle = document.getElementById("contact");
 
     contactArticle.style.opacity = '0';
     contactArticle.style.visibility = 'hidden';
     homeArticle.style.opacity = '0';
     homeArticle.style.visibility = 'hidden';
+    projectsArticle.style.opacity = '0';
+    projectsArticle.style.visibility = 'hidden';
 
     setTimeout(function () {
         workArticle.style.opacity = '1';
@@ -37,12 +43,15 @@ function revealWorkArticle(event) {
 function revealContactArticle(event) {
     const homeArticle = document.getElementById("home");
     const workArticle = document.getElementById("work");
+    const projectsArticle = document.getElementById("projects");
     const contactArticle = document.getElementById("contact");
     
     workArticle.style.opacity = '0';
     workArticle.style.visibility = 'hidden';
     homeArticle.style.opacity = '0';
     homeArticle.style.visibility = 'hidden';
+    projectsArticle.style.opacity = '0';
+    projectsArticle.style.visibility = 'hidden';
 
     setTimeout(function () {
         contactArticle.style.opacity = '1';
@@ -51,3 +60,33 @@ function revealContactArticle(event) {
     
 }
 
+function revealProjectsArticle() {
+    const homeArticle = document.getElementById("home");
+    const workArticle = document.getElementById("work");
+    const projectsArticle = document.getElementById("projects");
+    const contactArticle = document.getElementById("contact");
+    
+    workArticle.style.opacity = '0';
+    workArticle.style.visibility = 'hidden';
+    homeArticle.style.opacity = '0';
+    homeArticle.style.visibility = 'hidden';
+    contactArticle.style.opacity = '0';
+    contactArticle.style.visibility = 'hidden';
+
+    setTimeout(function () {
+        projectsArticle.style.opacity = '1';
+        projectsArticle.style.visibility = 'visible';
+    },500);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.image-info1').forEach(infoButton => {
+        infoButton.addEventListener('click', (event) => {
+            event.stopPropagation();
+            event.preventDefault();
+
+            const row = infoButton.closest('.row');
+            row.classList.toggle('active');
+        })
+    })
+})
